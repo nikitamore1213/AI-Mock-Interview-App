2b3ab076dc3f7bfe8f9f0726c26a9703bf57fe2d
nikitamore1213 <nikitamore.1228@gmail.com>
2025-09-06 19:47:55 +0530

    Changes: Project Initiated & Setup Completed with ShadCn UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,5 @@
-import React from 'react';
-
 const App = () => {
-  return (
-    <div className='text-blue-700'>App</div>
-  )
-}
+  return <div className="text-blue-500">App</div>;
+};
 
-export default App
+export default App;
